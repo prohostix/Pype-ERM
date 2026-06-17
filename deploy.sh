d0d0b29998f539f65c09bca0c0cd59d4ebc2c436
Prohostix <your-prohostix-email@example.com>
2026-06-17 12:34:22 +0530

    add socket.io WebSocket proxy configuration to Nginx sites in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -124,6 +124,16 @@ server {
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     }
+
+    location /socket.io/ {
+        proxy_pass http://localhost:6478/socket.io/;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
 }
 EOT
 
